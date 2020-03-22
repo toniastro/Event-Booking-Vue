c4c05a64f544e842f14832a78bcc5eeeaaa487a7
Iamt-chadwick <akinmolayanoluwatoni@gmail.com>
2020-03-22 22:29:48 +0100

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
-// import blog from '@/components/blog'
-// import services from '@/components/services'
-// import contact from '@/components/contact'
-// import details from '@/components/details'
 
 Vue.use(Router)
 
@@ -15,30 +11,5 @@ export default new Router({
       name: 'home',
       component: home
     },
-    // {
-    //   path: '/home',
-    //   name: 'home',
-    //   component: home
-    // },
-    // {
-    //   path: '/blog',
-    //   name: 'blog',
-    //   component: blog
-    // },
-    // {
-    //   path: '/services',
-    //   name: 'services',
-    //   component: services
-    // },
-    // {
-    //   path: '/contact',
-    //   name: 'contact',
-    //   component: contact
-    // },
-    // {
-    //   path: '/details/:Pid',
-    //   name: 'details',
-    //   component: details
-    // }
   ]
 })
