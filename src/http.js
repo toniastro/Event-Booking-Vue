b365af63139ae4c62ecf61f60015829a9d71c2f7
Akinmolayan Toni <akinmolayanoluwatoni@gmail.com>
2020-04-08 22:55:52 +0100

    Update http.js

--- src/http.js
+++ src/http.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://salty-river-82901.herokuapp.com/api/'
-export const API_publicKey = "FLWPUBK-093fc2a9316f439feaa7e8eb6d678c72-X"
+export const API_URL = 
+export const API_publicKey = 
